0783e23c4a2b04f90a35b3a68466a4afa88cc5c0
kaviyarajadurai-krh <kaviyarajadurai@gmail.com>
2026-07-16 13:54:29 +0530

    Add Spring REST using Spring Boot 3 project

---
+++ Spring REST using Spring Boot 3/spring-learn-country-service/src/main/java/com/cognizant/spring_learn/SpringLearnCountryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnCountryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnCountryServiceApplication.class, args);
+	}
+
+}
---
+++ Spring REST using Spring Boot 3/spring_learn_rest_hello/src/main/java/com/cognizant/springlearn/SpringLearnRestHelloApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnRestHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnRestHelloApplication.class, args);
+	}
+
+}
